236f3f96e8436c88ee70b071be4f8cc4b608f4e2
sahatilfat <sahatilfat@gmail.com>
2021-11-15 10:08:06 +0700

    simple routing

--- routes/api.php
+++ routes/api.php
@@ -20,12 +20,18 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/employees', [EmployeeController::class, 'index']);
+// Route::get('/employees', [EmployeeController::class, 'index']);
 
-Route::post('/employees', [EmployeeController::class, 'store']);
+// Route::post('/employees', [EmployeeController::class, 'store']);
 
-Route::get('/employees/{id}', [EmployeeController::class, 'show']);
+// Route::get('/employees/{id}', [EmployeeController::class, 'show']);
 
-Route::put('/employees/{id}', [EmployeeController::class, 'update']);
+// Route::put('/employees/{id}', [EmployeeController::class, 'update']);
 
-Route::delete('/employees/{id}', [EmployeeController::class, 'destroy']);
+// Route::delete('/employees/{id}', [EmployeeController::class, 'destroy']);
+
+// * selama kita menggunaan method bawaan yang sediakan oleh laravel, dan akses url nya tetap sama, maka kita dapat mempersingkat penulisan Route-nya
+// * dan kalau ada method  yg kita hapus, maka kita dapat menggunakan pengecualian seperti berikut ini :
+//* Route::resource('employees', [EmployeeController::class])->except(['create', 'edit']);
+
+Route::resource('/employees', [EmployeeController::class]);
